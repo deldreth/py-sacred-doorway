88725f5b4b24635ac4fd1ec5795af06c692cb831
deldreth <devin.eldreth@gmail.com>
2015-06-30 13:55:15 -0400

    Proper scrolling marquee style display for the flame file.

--- src/flame.py
+++ src/flame.py
@@ -8,10 +8,10 @@ from collections import deque
 sacred = Doorway()
 sacred.bow()
 
-sleep(1)
+sleep(2)
 
-img = Image.open("doorway/res/flames.jpeg")
-img = img.resize((29, 2048))
+# img = Image.open("doorway/res/flames.jpeg")
+# img = img.resize((29, 2048))
 
 #img = Image.open("doorway/res/rainbow.jpg")
 #img = img.resize((29, 2048))
@@ -23,51 +23,60 @@ img = img.resize((29, 2048))
 #img = Image.open("doorway/res/water2.jpg")
 #img = img.resize((29, 333))
 
-"""
-img = Image.open("doorway/res/metatron.jpg")
-img = img.resize((29, 1080))
-"""
+imgs = []
 
-pixels = img.load()
+img = Image.open("doorway/res/chakras/1.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/2.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/3.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/4.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/5.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/6.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
+
+img = Image.open("doorway/res/chakras/7.jpg")
+img = img.resize((29, 29))
+imgs.append(img)
 
 lines = []
-for y in range(img.size[1]):
-	xs = []
-	for x in range(img.size[0]):
-		xs.append(pixels[x, y])
+for img in imgs:
+	pixels = img.load()
+	for y in range(img.size[1]):
+		xs = []
+		for x in range(img.size[0]):
+			xs.append(pixels[x, y])
 
-	lines.append(xs)
-	del xs
+		lines.append(xs)
+		del xs
 
 deq = deque(maxlen=7)
 
-sheet = 1
-for line in lines:
-
-	if sheet > 7:
-		sheet = 1
-
-	deq.append(sheet)
-	sheet += 1
-
-	for sheet_deq in deq:
-		count = 0
-
-		for l, r in sacred.sheets[sheet_deq]:
-			sacred.pixels[l] = line[count]
-			sacred.pixels[r] = line[count]
-			count += 1
-
-	sacred.bow()
-	sleep(0.05)
-
-	"""
-	for l, r in sacred.sheets[sheet]:
-		sacred.pixels[l] = line[count]
-		sacred.pixels[r] = line[count]
-		count += 1
-
-	sacred.bow()
-	"""
-
-	print deq
+while True:
+	for line in lines:
+		deq.append(line)
+		sheet_count = 1
+		for deq_line in deq:
+			pix_count = 0
+			for l,r in sacred.sheets[sheet_count]:
+				sacred.pixels[l] = deq_line[pix_count]
+				sacred.pixels[r] = deq_line[pix_count]
+				pix_count += 1
+				
+			sheet_count += 1
+			sacred.bow()
+		sleep(0.05)
