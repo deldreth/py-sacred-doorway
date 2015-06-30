d943c7339df61d85d9d8124718e480e6ad065751
deldreth <devin.eldreth@gmail.com>
2015-06-30 11:15:19 -0400

    Updates to flame.

--- src/flame.py
+++ src/flame.py
@@ -3,16 +3,33 @@
 from doorway.doorway import Doorway
 from time import sleep
 from PIL import Image
+import Queue
 
 sacred = Doorway()
 sacred.bow()
 
-img = Image.open("doorway/res/flames.jpeg")
+sleep(1)
+
+#img = Image.open("doorway/res/flames.jpeg")
+#img = img.resize((29, 2048))
+
 #img = Image.open("doorway/res/rainbow.jpg")
-img = img.resize((29, 2048))
+#img = img.resize((29, 2048))
+
+
+#img = Image.open("doorway/res/wedding1.jpg")
+#img = img.resize((29, 960))
+
+img = Image.open("doorway/res/water2.jpg")
+img = img.resize((29, 333))
+
+"""
+img = Image.open("doorway/res/metatron.jpg")
+img = img.resize((29, 1080))
+"""
+
 pixels = img.load()
 
-lines = []
 for y in range(img.size[1]):
 	xs = []
 	for x in range(img.size[0]):
@@ -20,7 +37,7 @@ for y in range(img.size[1]):
 
 	lines.append(xs)
 
-
+#queue = Queue.Queue(7)
 for line in lines:
 	count = 0
 	for sp in sacred.sheets[1]:
@@ -30,7 +47,7 @@ for line in lines:
 	sacred.bow()
 
 	count = 0
-	for sp in sacred.sheets[2]:
+	for sp in (sacred.sheets[2]):
 		sacred.pixels[sp[0]] = line[count]
 		sacred.pixels[sp[1]] = line[count]
 		count += 1	
