1d6605533bef7fd3743aa0ccc1cce6647cdd7adf
deldreth <devin.eldreth@gmail.com>
2015-07-10 21:27:37 -0400

    Fixing all the craziness, using BaseManager to handle state maintenance between processes, and some mild performance fixes.

--- src/doorway/doorway.py
+++ src/doorway/doorway.py
@@ -1,6 +1,6 @@
 from sheet import Sheet
 from PIL import Image
-from time import sleep
+from time import sleep, time
 from Queue import Queue
 
 from collections import deque
@@ -20,11 +20,12 @@ class Doorway (object):
 		7 : 0
 	}
 
-	pixels  = [(0, 0, 0) for x in range(392)]
-	client  = opc.Client("localhost:7890")
+	# pixels  = [(0, 0, 0) for x in range(392)]
+	#client  = opc.Client("localhost:7890")
 
 	def __init__ (self, client = "localhost:7890"):
 		self.client = opc.Client(client)
+		self.pixels = [(0, 0, 0) for x in range(392)]
 
 		for s in self.sheets:
 			self.sheets[s] = Sheet(s, self.pixels)
@@ -38,7 +39,6 @@ class Doorway (object):
 		self.sheets[sheet].set(red, green, blue)
 
 	# Write the pixel state to the client
-	@classmethod
 	def bow (self, tsleep=0):
 		self.client.put_pixels(self.pixels, 0)
 		sleep(tsleep)
@@ -48,7 +48,31 @@ class Doorway (object):
 		self.bow()
 
 	def pics (self, path = "doorway/res/"):
-		return DoorwayImages(path).__iter__()
+		# imgs = DoorwayImages(path).__iter__()
+		deq  = deque(maxlen=7)
+
+		for line in DoorwayImages(path).__iter__():
+			deq.append(line)
+			sheet_count = 1
+
+			for deq_line in deq:
+				pix_count = 0
+				for l,r in self.sheets[sheet_count]:
+					self.pixels[l] = deq_line[pix_count]
+					self.pixels[r] = deq_line[pix_count]
+					pix_count += 1
+
+				sheet_count += 1
+			self.bow(1/15)
+
+	def get_sheets(self):
+		return self.sheets
+
+	def get_pixels(self):
+		return self.pixels
+
+	def set_pixel(self, index, value):
+		self.pixels[index] = value;
 
 	@classmethod
 	def color_wheel(self, value):
@@ -60,15 +84,17 @@ class Doorway (object):
 			return (255 - value * 3, 0, value * 3)
 		else:
 			value -= 170
-			return (0, value * 3, 255 - value * 3)	
+			return (0, value * 3, 255 - value * 3)
+
 
 
 class DoorwayImages (Doorway):
 	def __init__ (self, path = "doorway/res/"):
-		self.path = os.path.abspath(path) 
+		self.path = os.path.abspath(path)
 		self.imgs = []
+		self.deq  = deque(maxlen = 7)
 
-		for pic in os.listdir(self.path):
+		for pic in sorted(os.listdir(self.path)):
 			if os.path.isfile(path+pic):
 				img = Image.open(path+pic)
 				img = img.resize((28, img.size[1])) # Scale width down to 28 px but leave height alone
@@ -93,14 +119,152 @@ class DoorwayImages (Doorway):
 					xs.append(pixels[x, y])
 
 				lines.append(xs)
-				del xs	
+				del xs
 
 		del self.imgs
 
 		return lines
 
 
-class DoorwayAnimations (Doorway, threading.Thread): 
+
+class DoorwayEffects (Doorway):
+	"""
+		strobe_*    - Just that, it strobes all the sheets
+		FtoB & BtoF - Front to Back, Back to Front
+		rainbow_*   - Uses @Doorway.color_wheel to create rainbow effects
+		timed_      - Time is involved in some way
+		wipe_       - "wipes" a color up|down on a sheet
+	"""
+
+	#renderable = True
+
+	def __init__ (self):
+		super(DoorwayEffects, self).__init__()
+		self.renderable = True
+
+	def set_renderable(self, renderable):
+		self.renderable = renderable
+
+	def get_renderable(self):
+		return self.renderable
+
+	def all_black (self):
+		for sheet in range(1,8):
+			self.put(sheet, rgb=(0, 0, 0))
+		self.bow()
+
+	def strobe_FtoB (self, times=10, tsleep=0.01, color1=(255, 255, 255), color2=(0, 0, 0)):
+		for sheet in range(1,8):
+			for n in range(times):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=color1)
+				self.bow(tsleep)
+				self.put(sheet, rgb=color2)
+				self.bow(tsleep)
+
+	def strobe_BtoF (self, times=10, tsleep=0.01, color1=(255, 255, 255), color2=(0, 0, 0)):
+		for sheet in reversed(range(1,7)):
+			for n in range(times):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=color1)
+				self.bow(tsleep)
+				self.put(sheet, rgb=color2)
+				self.bow(tsleep)
+
+	def strobe_rainbow_FtoB (self, tsleep=0.1):
+		for color in range(1, 250, 20):
+			for sheet in range(1,8):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=Doorway.color_wheel(color))
+				self.bow(tsleep)
+				self.put(sheet, rgb=(0, 0, 0))
+				self.bow(tsleep)
+
+	def strobe_rainbow_BtoF (self, tsleep=0.1):
+		for color in range(1, 250, 20):
+			for sheet in reversed(range(1,8)):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=Doorway.color_wheel(color))
+				self.bow(tsleep)
+				self.put(sheet, rgb=(0, 0, 0))
+				self.bow(tsleep)
+
+	def rainbow_FtoB (self, tsleep=0.1):
+		for color in range(1, 250, 20):
+			for sheet in range(1,8):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=Doorway.color_wheel(color))
+				self.bow(tsleep)
+
+	def rainbow_BtoF (self, tsleep=0.1):
+		for color in range(1, 250, 20):
+			for sheet in reversed(range(1,8)):
+				if not self.renderable:
+					return True
+
+				self.put(sheet, rgb=Doorway.color_wheel(color))
+				self.bow(tsleep)
+
+	def timed_rainbow_FtoB (self):
+		for color in range(1, 250, 10):
+			for sheet in range(1,8):
+				for ti in range(1, 30):
+					if not self.renderable:
+						return True
+
+					self.put(sheet, rgb=Doorway.color_wheel(color))
+					self.bow(ti/30)
+
+	def timed_rainbow_BtoF (self):
+		for color in range(1, 250, 10):
+			for sheet in reversed(range(1,8)):
+				for ti in range(1, 30):
+					if not self.renderable:
+						return True
+
+					self.put(sheet, rgb=Doorway.color_wheel(color))
+					self.bow(ti/30)
+
+	def wipe_down (self, sheet, color=(255, 255, 255), tsleep=0.01):
+		for l, r in self.sheets[sheet]:
+			if not self.renderable:
+				return True
+
+			self.pixels[l] = color
+			self.pixels[r] = color
+			self.bow(tsleep)
+
+	def wipe_up (self, sheet, color=(255, 255, 255), tsleep=0.01):
+		for l, r in reversed(list(self.sheets[sheet])):
+			if not self.renderable:
+				return True
+
+			self.pixels[l] = color
+			self.pixels[r] = color
+			self.bow(tsleep)
+
+	def wipe_left_down (self, color=(255, 255, 255), tsleep=0.05):
+		for sheet in self.sheets:
+			for l, r in self.sheets[sheet]:
+				if not self.renderable:
+					return True
+
+				self.pixels[l] = color
+				self.bow(tsleep)
+
+
+
+class DoorwayAnimations (Doorway, threading.Thread):
 	animations = Queue() # .put({'sheets' : (n,), 'rgb' : (red, green, blue), 'direction' : 'up|down', 'sleep' : n})
 
 	def __init__ (self, q):
@@ -108,10 +272,9 @@ class DoorwayAnimations (Doorway, threading.Thread):
 		self.animations = q
 
 	def run (self):
-		print self.animations.qsize()
 		while True:
 			animation = self.animations.get()
-			
+
 			if animation:
 				for sheet in animation['sheets']:
 					if 'direction' in animation and animation['direction'] == 'up':
--- src/sacred-doorway.py
+++ src/sacred-doorway.py
@@ -8,44 +8,23 @@ from PIL import Image
 
 import multiprocessing
 
-from doorway.doorway import Doorway
+from multiprocessing.managers import BaseManager
 
-cam     = Camera(1, threaded=False, prop_set={"width":128, "height":96}) # Threaded by default, and boy if it doesn't freak everything out. Something something... multiple processes grabbing thread data at a time, wooo
+from doorway.doorway import Doorway, DoorwayEffects
+
+cam = Camera(0, prop_set={"width":128, "height":96}) # Threaded by default, and boy if it doesn't freak everything out. Something something... multiple processes grabbing thread data at a time, wooo
 # display = Display((640, 480))
 
-sacred  = Doorway()
-
-def proc_camera (manager_dict):
+def proc_camera (manager_dict, sacred):
 	"""
 	Camera handling PROCESS. If the manager dictionary says there is light...
 	unthreaded buffer an image from the camera and display.
 	"""
-	global sacred
 
 	def draw_camera (sacred, image):
-		# Process transition animation
-		if not manager_dict['has_light']:
-			sacred.put(2, rgb=(0, 0, 0))
-			sacred.put(3, rgb=(0, 0, 0))
-			sacred.put(4, rgb=(0, 0, 0))
-			sacred.put(5, rgb=(0, 0, 0))
-			sacred.put(6, rgb=(0, 0, 0))
-			sacred.put(7, rgb=(0, 0, 0))
-
-			start = time()
-			while time() - start <= 3:
-				sacred.put(1, rgb=(255, 255, 255))
-				sacred.bow(0.5)
-				sacred.put(1, rgb=(0, 0, 0))
-				sacred.bow(0.5)
-
-		 	return
-
 		pilImage = image.rotate(90).resize((28, 7))
 		pixels   = pilImage.load()
 
-		#image.save(display)
-		#sleep(0.04) #Eh?
 		lines = []
 		for y in range(pilImage.size[1]):
 			xs = []
@@ -62,9 +41,9 @@ def proc_camera (manager_dict):
 			sheet_count = 1
 			for deq_line in deq:
 				pix_count = 0
-				for l,r in sacred.sheets[sheet_count]:
-					sacred.pixels[l] = deq_line[pix_count]
-					sacred.pixels[r] = deq_line[pix_count]
+				for l,r in sacred.get_sheets()[sheet_count]:
+					sacred.set_pixel(l, deq_line[pix_count])
+					sacred.set_pixel(r, deq_line[pix_count])
 					pix_count += 1
 
 				sheet_count += 1
@@ -73,58 +52,64 @@ def proc_camera (manager_dict):
 	while True:
 		if manager_dict['has_light'] and time() - manager_dict['cam_timer'] >= 5:
 			""" It has been 5 seconds, is there still a light source? """
+			sacred.set_renderable(False)
 			if manager_dict['image'] != 0:
 				draw_camera(sacred, manager_dict['image'])
 			else:
-				print "No image!"
-
 		else:
-			print "No camera."
+			sacred.set_renderable(True)
 			sleep(1)
 
 
 
 
-def proc_animation (manager_dict):
+def proc_animation (manager_dict, sacred):
+	where = [0]
 
-	global sacred
+	presentation = [
+		{'rainbow_FtoB' : [ 0.01 ] },
+		{'rainbow_BtoF' : [ 0.01 ] },
+		{'wipe_down' : [ 1 ] },
+		{'wipe_up' : [ 2 ] },
+		{'wipe_down' : [ 3 ] },
+		{'wipe_up' : [ 4 ] },
+		{'wipe_down' : [ 5 ] },
+		{'wipe_up' : [ 6 ] },
+		{'wipe_down' : [ 7 ] },
+		{'strobe_FtoB' : [] },
+		]
+
+	for _ in range(2):
+		presentation.append({'strobe_rainbow_FtoB' : []})
+		presentation.append({'strobe_rainbow_BtoF' : []})
+
+	for _ in range(2):
+		presentation.append({'rainbow_FtoB' : []})
+		presentation.append({'rainbow_BtoF' : []})
+
+	random.shuffle(presentation)
 
 	def draw_animation (sacred):
-		for x in range(256):
-			# Process transition animation
-			if manager_dict['has_light']:
-				sacred.put(2, rgb=(0, 0, 0))
-				sacred.put(3, rgb=(0, 0, 0))
-				sacred.put(4, rgb=(0, 0, 0))
-				sacred.put(5, rgb=(0, 0, 0))
-				sacred.put(6, rgb=(0, 0, 0))
-				sacred.put(7, rgb=(0, 0, 0))
+		if where[0] == len(presentation) - 1:
+			random.shuffle(presentation)
+			where[0] = 0
 
-				start = time()
-				while time() - start <= 3:
-					sacred.put(1, rgb=(255, 255, 255))
-					sacred.bow(0.5)
-					sacred.put(1, rgb=(0, 0, 0))
-					sacred.bow(0.5)
+		for i, x in enumerate(presentation):
+			if i < where[0]:
+				continue
 
-			 	return
+			where[0] = i
+			for key, val in x.iteritems():
+				if getattr(sacred, key)(*val):
+					return
 
-			sacred.put(1, rgb = Doorway.color_wheel(x))
-			sacred.put(2, rgb = Doorway.color_wheel(x+10))
-			sacred.put(3, rgb = Doorway.color_wheel(x+20))
-			sacred.put(4, rgb = Doorway.color_wheel(x+30))
-			sacred.put(5, rgb = Doorway.color_wheel(x+20))
-			sacred.put(6, rgb = Doorway.color_wheel(x+10))
-			sacred.put(7, rgb = Doorway.color_wheel(x))
-			sacred.bow(0.01)
+		#sacred.pics(path="Doorwayrway/res/chakras/")
 
 	while True:
 		if not manager_dict['has_light'] and time() - manager_dict['ani_timer'] >= 5:
 			draw_animation(sacred)
 		else:
-			print "No animation."
 			sleep(1)
-
 
 
 
@@ -144,7 +129,7 @@ def thread_control (d):
 		h, l, s = img.toHLS().splitChannels()
 		l = l.threshold(150)
 
-		# blobs = l.findBlobs(150, minsize=5)
+		# blobs = l.findBlobs(150, minsize=5) # For a capture, like, 640 x 480
 
 		blobs = l.findBlobs(minsize=2)
 		if blobs:
@@ -180,20 +165,26 @@ def thread_control (d):
 
 			if not d['ani_timer']:
 				d['ani_timer'] = time()
+			sleep(1)
 
 		if x > 250:
 			x = 0
 
 		sleep(1/30)
 
+bm = BaseManager()
+bm.register('DoorwayEffects', DoorwayEffects)
+bm.start()
+sacred = bm.DoorwayEffects()
+
 manager  = multiprocessing.Manager()
 d = manager.dict({'has_light' : False, 'image' : 0, 'cam_timer' : 0, 'ani_timer' : 5})
 
-camera = multiprocessing.Process(target=proc_camera, args=(d,))
+camera = multiprocessing.Process(target=proc_camera, args=(d, sacred, ))
 camera.daemon = True
 camera.start()
 
-animation = multiprocessing.Process(target=proc_animation, args=(d,))
+animation = multiprocessing.Process(target=proc_animation, args=(d, sacred, ))
 animation.daemon = True
 animation.start()
 
